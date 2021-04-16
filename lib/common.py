345815165c40b10598617ea937499a032527dcd8
Robin Law <manmanluoluo@gmail.com>
2021-04-16 17:54:37 +0800

    common commit

--- lib/common.py
+++ lib/common.py
@@ -8,6 +8,12 @@ conda activate your_env_name
 conda deactivate
 conda create --name new_env_name --clone old_env_name
 conda remove --name your_env_name --all
+
+git branch -m master main
+git fetch origin
+git branch -u origin/main main
+git remote set-head origin -a
+
 """
 
 
